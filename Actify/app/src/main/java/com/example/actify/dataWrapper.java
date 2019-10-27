a56db5141cd23173e0b68acef2c9560d6b14b01c
Lai Wei Hong <weihong.lai@github.com>
2019-10-27 06:50:59 +0000

    Finalised UI for homepage

--- Actify/app/src/main/java/com/example/actify/dataWrapper.java
+++ Actify/app/src/main/java/com/example/actify/dataWrapper.java
@@ -1,125 +1,125 @@
-package com.example.actify;
-
-import android.util.Log;
-
-import androidx.annotation.NonNull;
-
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.QueryDocumentSnapshot;
-import com.google.firebase.firestore.QuerySnapshot;
-
-import java.util.ArrayList;
-import java.util.List;
-
-public class dataWrapper {
-    // Create a Cloud Firebase Instance
-    FirebaseFirestore db = FirebaseFirestore.getInstance();
-    private static final String TAG = "dataWrapper";
-    private List result
-
-    // Method to get all events
-    // @return: An object with attributes(date, description, location, picture, title)
-    public void getAllEvents(List results) {
-        db.collection("events")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                results.add(document.getData());
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-    }
-
-    // Method to get all event users
-    // @return: An map that maps user id to attendance
-    // @input: event id
-    public void getAllEventUsers(List results) {
-        db.collection("eventusers")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-                                results.add(document.getData());
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-    }
-
-    // Method to get all survey answers
-    public void getAllSurveyAnswers(List results) {
-        db.collection("surveyanswers")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                results.add(document.getData());
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-    }
-
-    // Method to get survey questions
-    public void getAllSurveys(List results) {
-        db.collection("surveys")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                results.add(document.getData());
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-    }
-
-    public void getAllUsers(List results) {
-        db.collection("users")
-                .get()
-                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
-                        if (task.isSuccessful()) {
-                            for (QueryDocumentSnapshot document : task.getResult()) {
-
-                                results.add(document.getData());
-                                Log.d(TAG, document.getId() + " => " + document.getData());
-                            }
-
-                        } else {
-                            Log.w(TAG, "Error getting documents.", task.getException());
-                        }
-                    }
-                });
-    }
-
-}
+//package com.example.actify;
+//
+//import android.util.Log;
+//
+//import androidx.annotation.NonNull;
+//
+//import com.google.android.gms.tasks.OnCompleteListener;
+//import com.google.android.gms.tasks.Task;
+//import com.google.firebase.firestore.FirebaseFirestore;
+//import com.google.firebase.firestore.QueryDocumentSnapshot;
+//import com.google.firebase.firestore.QuerySnapshot;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class dataWrapper {
+//    // Create a Cloud Firebase Instance
+//    FirebaseFirestore db = FirebaseFirestore.getInstance();
+//    private static final String TAG = "dataWrapper";
+//    private List result;
+//
+//    // Method to get all events
+//    // @return: An object with attributes(date, description, location, picture, title)
+//    public void getAllEvents(List results) {
+//        db.collection("events")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                results.add(document.getData());
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//    // Method to get all event users
+//    // @return: An map that maps user id to attendance
+//    // @input: event id
+//    public void getAllEventUsers(List results) {
+//        db.collection("eventusers")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//                                results.add(document.getData());
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//    // Method to get all survey answers
+//    public void getAllSurveyAnswers(List results) {
+//        db.collection("surveyanswers")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                                results.add(document.getData());
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//    // Method to get survey questions
+//    public void getAllSurveys(List results) {
+//        db.collection("surveys")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                                results.add(document.getData());
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//    public void getAllUsers(List results) {
+//        db.collection("users")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//                                results.add(document.getData());
+//                                Log.d(TAG, document.getId() + " => " + document.getData());
+//                            }
+//
+//                        } else {
+//                            Log.w(TAG, "Error getting documents.", task.getException());
+//                        }
+//                    }
+//                });
+//    }
+//
+//}
