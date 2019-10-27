da56c89430e5eb6bee9c7882c3e3e68e4112b9a2
Lai Wei Hong <weihong.lai@github.com>
2019-10-27 05:48:00 +0000

    Completed homepage UI

---
+++ Actify/app/src/main/java/com/example/actify/dataWrapper.java
@@ -0,0 +1,11 @@
+package com.example.actify;
+
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class dataWrapper {
+    // Create a Cloud Firebase Instance
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private static final String TAG = "dataWrapper";
+
+    // Method 
+}
